b48e24b4d789c98eadfb34dc4839e441f54730f3
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-16 14:57:51 +0200

    Added response objects for user

---
+++ src/main/java/com/booleanuk/api/response/UserListResponse.java
@@ -0,0 +1,8 @@
+package com.booleanuk.api.response;
+
+import com.booleanuk.api.model.User;
+
+import java.util.List;
+
+public class UserListResponse extends Response<List<User>> {
+}
---
+++ src/main/java/com/booleanuk/api/response/UserResponse.java
@@ -0,0 +1,6 @@
+package com.booleanuk.api.response;
+
+import com.booleanuk.api.model.User;
+
+public class UserResponse extends Response<User> {
+}
