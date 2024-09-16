5abba24ef5c1e4618718141a33c7d1c2a3ce0593
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-16 14:57:40 +0200

    Added user model

---
+++ src/main/java/com/booleanuk/api/model/User.java
@@ -0,0 +1,33 @@
+package com.booleanuk.api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String username;
+
+    @Column
+    private String email;
+
+    public User(int id) {
+        this.id = id;
+    }
+}
