2e768b5e00d7cdeb0c254d74c992b7d7ff8de8af
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-16 14:39:23 +0200

    added csrf.disable() to security config

--- src/main/java/com/booleanuk/api/config/SecurityConfiguration.java
+++ src/main/java/com/booleanuk/api/config/SecurityConfiguration.java
@@ -16,6 +16,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/games", "/games/*").authenticated())
                 .formLogin((form) -> form
