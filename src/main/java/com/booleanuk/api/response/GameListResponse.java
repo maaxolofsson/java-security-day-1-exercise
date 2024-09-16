59bbf411d88b84e8c39cdffe0222f60aad7cb57c
Max V Olofsson <max.olofsson@se.experis.com>
2024-09-16 13:27:04 +0200

    Created response objects (taken from previous workshops/exercises)

---
+++ src/main/java/com/booleanuk/api/response/GameListResponse.java
@@ -0,0 +1,8 @@
+package com.booleanuk.api.response;
+
+import com.booleanuk.api.model.Game;
+
+import java.util.List;
+
+public class GameListResponse extends Response<List<Game>> {
+}
---
+++ src/main/java/com/booleanuk/api/response/GameResponse.java
@@ -0,0 +1,6 @@
+package com.booleanuk.api.response;
+
+import com.booleanuk.api.model.Game;
+
+public class GameResponse extends Response<Game> {
+}
